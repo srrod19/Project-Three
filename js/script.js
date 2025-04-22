9dcad577ede2a6eb2eb1bd5033830845074422be
srrod19 <srodriguez10@hawk.iit.edu>
2025-04-21 22:38:44 -0500

    changed map

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 41.830131 , lng: -87.627107 },
         zoom: 4
     });
