c78ed52faa942328226f5f90aa09b5493a92564c
srrod19 <srodriguez10@hawk.iit.edu>
2025-04-21 22:43:32 -0500

    added comma

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var initMap;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 41.830131 , lng: -87.627107 },
-        zoom: 4
+        zoom: 4,
     });
 
 }
