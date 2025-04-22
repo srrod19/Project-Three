9c91af106918c751f5d20567f132ed2635b7dbfd
srrod19 <srodriguez10@hawk.iit.edu>
2025-04-21 23:34:24 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 function map() {
     const loc = {lat:41, lng:-87};
   
-    const maps = new google.maps.Map(document.getElementById("map"), {
-      zoom:5,
-      center:loc,
-      zoomControl: true,
-      mapTypeId:'roadmap',
-    });
 }
