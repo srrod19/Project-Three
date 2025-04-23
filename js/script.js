907b46e4833fd504b96215f3e2e69af17a8712ea
srrod19 <srodriguez10@hawk.iit.edu>
2025-04-23 10:26:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-function map() {
-    const loc = {lat:41, lng:-87};
-  
+function initMap() {
+    const defaultLocation = { lat: 40.7128, lng: -74.0060 }; // New York City
+    
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: defaultLocation,
+        mapTypeId: 'roadmap'
+    });
+
+
+map.addListener("click", (event) => {
+    addMarker(event.latLng, map);
+});
+
 }
